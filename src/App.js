14b526de042aad3a7434ec2f6b11a9b6fee147ab
mohammedfazil196@gmail.com <mohammedfazil196@gmail.com>
2022-03-30 15:45:35 +0530

    added alert message for both dark and light mode

--- src/App.js
+++ src/App.js
@@ -2,19 +2,35 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
+import Alert from './components/Alert';
 
  
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+const[alert,setAlert]= useState(null);
+const showAlert = (message , type)=>{
+setAlert({
+  msg:message, 
+  type:type
+})
+setTimeout(()=>{
+  setAlert(null);
+},3000);
+}
+
 
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode is enabled","sucess");
+   
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode is enabled","sucess");
+    
     }
   }
   return (
@@ -22,6 +38,7 @@ function App() {
     {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
     {/* <Navbar/> */}
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert} />
     <div className="container my-3">
     <TextForm heading="Enter the text to analyze below" mode={mode}/>
     </div>
